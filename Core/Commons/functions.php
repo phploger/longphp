d293cfadf5781a7edb230a672f2337aa2040368e
phploger <phploger@163.com>
2016-10-14 16:34:59 +0800

    Signed-off-by: phploger <phploger@163.com>

---
+++ Core/Commons/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function p($in){
+	if(is_number($in) || is_string($in)){
+		var_dump($in);
+	}else if(){
+		
+	}
+}
---
+++ Core/longphp.php
@@ -0,0 +1,7 @@
+<?php
+
+defined("ROOT_PATH") OR define("ROOT_PATH",dirname($_SERVER['SCRIPT_FILENAME']).'/');
+
+
+
+echo dirname($_SERVER['SCRIPT_FILENAME']).'/';
